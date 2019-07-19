eb1df494208141ba8e242e0fe1572a81a8ab367d
feven <feven.m.woldu@gmail.com>
2019-07-19 14:06:50 +0300

    created my models for image,location and category

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    Loc_name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.Loc_name
+
+    def save_Location(self):
+        self.save()
+
+class Category(models.Model):
+    Cat_name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.Cat_name
+
+    def save_category(self):
+        self.save()
+
+class Image(models.Model):
+    Img_name = models.CharField(max_length =30)
+    Img_description = models.CharField(max_length =200)
+    image = models.ImageField(upload_to = 'photos/')
+    category = models.ForeignKey(Category)
+    
+
+    def __str__(self):
+        return self.Img_name
+
+    def save_Img(self):
+        self.save()
+    
